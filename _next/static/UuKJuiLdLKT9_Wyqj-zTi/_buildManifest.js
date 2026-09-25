57b5a544026b3d9db9c6ccb99929a93f45aec531
vernus <a840683056@outlook.com>
2026-09-25 12:58:24 +0800

    chore: deploy static site 2026-09-25 04:58:24

---
+++ _next/static/UuKJuiLdLKT9_Wyqj-zTi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-0f13a29f211bf775.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
